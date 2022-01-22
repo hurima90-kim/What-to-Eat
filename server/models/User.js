02639282f1036cef6b7c535ad213158853665eb1
hurima90-kim <hurima90@gmail.com>
2022-01-22 15:34:08 +0900

    feat: create user schema & models with mongodb

---
+++ server/models/User.js
@@ -0,0 +1,36 @@
+const { MongoClient } = require("mongodb");
+
+const userSchema = MongoClient.Schema({
+  name: {
+    type: String,
+    maxlength: 50,
+  },
+  email: {
+    type: String,
+    trim: true,
+    unique: 1,
+  },
+  password: {
+    type: String,
+    minlength: 5,
+  },
+  lastname: {
+    type: String,
+    maxlength: 50,
+  },
+  role: {
+    type: Number,
+    default: 0,
+  },
+  image: String,
+  token: {
+    type: String,
+  },
+  tokenExp: {
+    type: Number,
+  },
+});
+
+const User = MongoClient.model("User", userSchema);
+
+module.exports = { User };
