d3f9afac4aa158fb7690976a323dfa805a2755fd
hurima90-kim <hurima90@gmail.com>
2022-01-23 22:31:03 +0900

    feat: setting http-proxy-middleware

--- client/src/pages/WelcomePage.js
+++ client/src/pages/WelcomePage.js
@@ -1,5 +1,10 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import axios from 'axios';
 
 export default function WelcomePage() {
-  return <React.Fragment></React.Fragment>;
+  useEffect(() => {
+    axios.get('/api/hello').then((response) => console.log(response));
+  }, []);
+
+  return <React.Fragment>메인 페이지</React.Fragment>;
 }
--- server/index.js
+++ server/index.js
@@ -1,11 +1,11 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 const port = 5001;
-const bodyParser = require("body-parser");
-const cookieParser = require("cookie-parser");
-const config = require("./config/key");
-const { auth } = require("./middleware/auth");
-const { User } = require("./models/User");
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const config = require('./config/key');
+const { auth } = require('./middleware/auth');
+const { User } = require('./models/User');
 
 // application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -15,18 +15,22 @@ app.use(bodyParser.json());
 app.use(cookieParser());
 
 // const { MongoClient } = require("mongodb");
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 mongoose
   .connect(config.mongoURI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
-  .then(() => console.log("Mongo DB Connected...!"))
+  .then(() => console.log('Mongo DB Connected...!'))
   .catch((err) => console.log(err));
 
-app.get("/", (req, res) => res.send("Hello World!"));
+app.get('/', (req, res) => res.send('Hello World!'));
 
-app.post("/api/users/register", (req, res) => {
+app.get('/api/hello', (req, res) => {
+  res.send('Hello Everyone');
+});
+
+app.post('/api/users/register', (req, res) => {
   // When you get the infomation need to sign up from the client, put them in the Database
 
   const user = new User(req.body);
@@ -36,13 +40,13 @@ app.post("/api/users/register", (req, res) => {
   });
 });
 
-app.post("/api/users/login", (req, res) => {
+app.post('/api/users/login', (req, res) => {
   // The requested email is found in the database.
   User.findOne({ email: req.body.email }, (err, user) => {
     if (!user) {
       return res.json({
         loginSuccess: false,
-        message: "제공된 이메일에 해당하는 유저가 없습니다.",
+        message: '제공된 이메일에 해당하는 유저가 없습니다.',
       });
     }
     // If the requested email is in the database, check if the password is correct.
@@ -50,7 +54,7 @@ app.post("/api/users/login", (req, res) => {
       if (!isMatch)
         return res.json({
           loginSuccess: false,
-          message: "비밀번호가 틀렸습니다.",
+          message: '비밀번호가 틀렸습니다.',
         });
       // If the password matches, create a token.
       user.generateToken((err, user) => {
@@ -58,7 +62,7 @@ app.post("/api/users/login", (req, res) => {
 
         // Save token in cookie.
         res
-          .cookie("x_auth", user.token)
+          .cookie('x_auth', user.token)
           .status(200)
           .json({ loginSuccess: true, userId: user._id });
       });
@@ -66,7 +70,7 @@ app.post("/api/users/login", (req, res) => {
   });
 });
 
-app.get("/api/users/auth", auth, (req, res) => {
+app.get('/api/users/auth', auth, (req, res) => {
   res.status(200).json({
     _id: req.user._id,
     isAdmin: req.user.role === 0 ? false : true,
@@ -79,8 +83,8 @@ app.get("/api/users/auth", auth, (req, res) => {
   });
 });
 
-app.get("/api/users/logout", auth, (req, res) => {
-  User.findOneAndUpdate({ _id: req.user._id }, { token: "" }, (err, user) => {
+app.get('/api/users/logout', auth, (req, res) => {
+  User.findOneAndUpdate({ _id: req.user._id }, { token: '' }, (err, user) => {
     if (err) return res.json({ success: false, err });
     return res.status(200).send({
       success: true,
