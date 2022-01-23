7de9e66570efeb395279b7323204177feb900a71
hurima90-kim <hurima90@gmail.com>
2022-01-23 21:46:10 +0900

    feat: apply react-router v6 & create new pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,19 @@
-import "./App.css";
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
+import WelcomePage from './pages/WelcomePage';
 
 function App() {
-  return <div></div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/" element={<WelcomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
