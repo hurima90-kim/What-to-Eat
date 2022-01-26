b9413977e693ad29c00cf444b81d8a78ecaa9b35
hurima90-kim <hurima90@gmail.com>
2022-01-26 22:02:42 +0900

    feat: create logout function

--- client/src/pages/WelcomePage.js
+++ client/src/pages/WelcomePage.js
@@ -1,10 +1,28 @@
 import React, { useEffect } from 'react';
 import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 export default function WelcomePage() {
+  const navigate = useNavigate();
+
   useEffect(() => {
     axios.get('/api/hello').then((response) => console.log(response));
   }, []);
 
-  return <React.Fragment>메인 페이지</React.Fragment>;
+  const onClickHandler = () => {
+    axios.get(`/api/users/logout`).then((response) => {
+      if (response.data.success) {
+        navigate('/login');
+      } else {
+        alert('로그아웃하는데 실패했습니다.');
+      }
+    });
+  };
+
+  return (
+    <React.Fragment>
+      메인 페이지
+      <button onClick={onClickHandler}>Logout</button>
+    </React.Fragment>
+  );
 }
