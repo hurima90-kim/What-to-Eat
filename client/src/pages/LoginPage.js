e0d114fbce602762a7e35cc52a4ca2c835dde4cb
hurima90-kim <hurima90@gmail.com>
2022-01-28 23:41:59 +0900

    feat: apply mui system in login page

--- client/src/pages/LoginPage.js
+++ client/src/pages/LoginPage.js
@@ -2,6 +2,19 @@ import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { loginUser } from '../_actions/user_action';
 import { useNavigate } from 'react-router-dom';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import {
+  TextField,
+  Button,
+  Checkbox,
+  FormControlLabel,
+  Grid,
+  Link,
+  Typography,
+  Avatar,
+  Box,
+  Container,
+} from '@mui/material';
 
 function LoginPage() {
   const dispatch = useDispatch();
@@ -35,16 +48,75 @@ function LoginPage() {
   };
 
   return (
-    <React.Fragment>
-      <form onSubmit={onSubmitHandler}>
-        <label>Email</label>
-        <input type="email" value={Email} onChange={onEmailHandler} />
-        <label>Password</label>
-        <input type="password" value={Password} onChange={onPasswordHandler} />
-        <br />
-        <button type="submit">Login</button>
-      </form>
-    </React.Fragment>
+    <Container component="main" maxWidth="xs">
+      <Box
+        sx={{
+          marginTop: 8,
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+        }}
+      >
+        <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Sign in
+        </Typography>
+        <Box
+          component="form"
+          onSubmit={onSubmitHandler}
+          noValidate
+          sx={{ mt: 1 }}
+        >
+          <TextField
+            label="Email Address"
+            name="Email"
+            required
+            fullWidth
+            autoComplete="email"
+            autoFocus
+            margin="normal"
+            onChange={onEmailHandler}
+          />
+          <TextField
+            label="Password"
+            name="Password"
+            type="password"
+            required
+            fullWidth
+            autoComplete="cureent-password"
+            margin="normal"
+            onChange={onPasswordHandler}
+          />
+          <FormControlLabel
+            control={<Checkbox value="remember" color="primary" />}
+            label="Remember me"
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            // mt: margin-top , mb: margin-bottom
+            sx={{ mt: 3, mb: 2 }}
+          >
+            Sign In
+          </Button>
+          <Grid container>
+            <Grid item xs>
+              <Link href="/" variant="body2">
+                Forgot password?
+              </Link>
+            </Grid>
+            <Grid item>
+              <Link href="/register" variant="body2">
+                {"Don't have an account? Sign Up"}
+              </Link>
+            </Grid>
+          </Grid>
+        </Box>
+      </Box>
+    </Container>
   );
 }
 
